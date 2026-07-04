329cece78418e9a45bcf2955f95a8448047f55dc
Sreehari Annam <sannam@wescodist.com>
2026-07-04 09:49:57 -0400

    batch 6: next cities

--- cities.js
+++ cities.js
@@ -1,20 +1,9 @@
 const massachusettsCities = [
-  "Somerset", "Somerville", "South Chatham", "South Dartmouth", "South Deerfield",
-  "South Dennis", "South Easton", "South Egremont", "South Grafton", "South Hadley",
-  "South Hamilton", "South Harwich", "South Lancaster", "South Lee", "South Walpole",
-  "South Wellfleet", "South Weymouth", "South Yarmouth", "Southampton", "Southborough",
-  "Southbridge", "Southfield", "Southwick", "Spencer", "Springfield",
-  "Sterling", "Still River", "Stoneham", "Stoughton", "Stow",
-  "Sturbridge", "Sudbury", "Sunderland", "Sutton", "Swampscott",
-  "Swansea", "Taunton", "Templeton", "Tewksbury", "Thorndike",
-  "Three Rivers", "Topsfield", "Townsend", "Turners Falls", "Tyngsboro",
-  "Upton", "Uxbridge", "Vineyard Haven", "Waban", "Wakefield",
-  "Wales", "Walpole", "Waltham", "Ware", "Wareham",
-  "Warren", "Watertown", "Waverley", "Wayland", "Webster",
-  "Wellesley", "Wellesley Hills", "Wellfleet", "Wenham", "West Barnstable",
-  "West Boylston", "West Bridgewater", "West Brookfield", "West Chatham", "West Dennis",
-  "West Falmouth", "West Harwich", "West Hyannisport", "West Newbury", "West Newton",
-  "West Roxbury", "West Springfield", "West Stockbridge", "West Townsend", "West Wareham",
+  "West Yarmouth", "Westborough", "Westfield", "Westford", "Westminster",
+  "Weston", "Westport", "Westwood", "Weymouth", "Whately",
+  "Whitinsville", "Whitman", "Wilbraham", "Williamsburg", "Williamstown",
+  "Wilmington", "Winchendon", "Winchester", "Winthrop", "Woburn",
+  "Woodville", "Worcester", "Worthington", "Wrentham", "Yarmouth Port",
 ];
 
 const BATCH_SIZE = 5;
